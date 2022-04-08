3dd1fd0d579b91f39a6dcf88ff0f6655cbad6937
saldermorth <berg_gustav@hotmail.com>
2022-04-08 08:46:35 +0200

    Create I product service klar

---
+++ assignment_Dataaccess/Models/Vendors.cs
@@ -0,0 +1,9 @@
+﻿namespace assignment_Dataaccess.Models
+{
+    public class Vendors
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        
+    }
+}
